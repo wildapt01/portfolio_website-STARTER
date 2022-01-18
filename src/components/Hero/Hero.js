d39d7b6deb12b2b261634e497194986be65d5ca9
Patrick_B <pb5191@gmail.com>
2022-01-17 20:07:31 -0500

    add Hero title and text

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,6 +8,23 @@ import {
 import Button from "../../styles/GlobalComponents/Button";
 import { LeftSection } from "./HeroStyles";
 
-const Hero = (props) => <div>Hero</div>;
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        I am a Software Developer well versed in NodeJS, React, Javascript, CSS
+        and HTML among other technologies. At the Literacy Assitance Center, my
+        main project is a complete overhaul of ASISTS, our main product...
+      </SectionText>
+      <Button onClick={() => (window.location = "https://google.com")}>
+        Learn more
+      </Button>
+    </LeftSection>
+  </Section>
+);
 
 export default Hero;
